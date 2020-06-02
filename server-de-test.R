4fc88286aeaf9737e48be134afbb33dd42b46ad2
Adam <adam.thomas.little@icloud.com>
2020-06-02 16:06:00 -0700

    Improvements, but download still non-functional.

--- server-de-test.R
+++ server-de-test.R
@@ -5,15 +5,27 @@
   #updateSelectizeInput(session,'sel_gene', choices= data_analyzedgenes,server=TRUE)
 
   output$detable <- renderTable({
-    if (input$cell_type_1 == input$cell_type_2) {
-      "Please select two different PBMC cell types."
-    } else {
+    
       FindMarkers(pbmc, 
                   ident.1 = input$cell_type_1, 
                   ident.2 = input$cell_type_2, 
                   features = input$select_gene,
                   test.use = input$test_type,
                   verbose=TRUE
-                  )
+                  )[1:input$n_select,]
+  
+  },
+    rownames = TRUE,
+    striped = TRUE,
+    bordered = TRUE,
+    hover = TRUE)
+  
+    # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("pbmc-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(data, file)
     }
-  })
+  )
--- ui-tab-de-test.R
+++ ui-tab-de-test.R
@@ -11,7 +11,8 @@ tabPanel("DE Tests",
       selectInput("cell_type_1",
                   label = "Select First PBMC Subtype",
                   choices = levels(pbmc),
-                  multiple = FALSE
+                  multiple = FALSE,
+                  selected = "CD14+ Mono"
         ) %>% 
         shinyInput_label_embed(
           shiny_iconlink() %>%
@@ -23,6 +24,7 @@ tabPanel("DE Tests",
                   label="Select Second PBMC Subtype",
                   choices = levels(pbmc),
                   multiple = FALSE,
+                  selected = "FCGR3A+ Mono"
         ) %>% 
         shinyInput_label_embed(
           shiny_iconlink() %>%
@@ -30,17 +32,6 @@ tabPanel("DE Tests",
               title = "Letter", content = "Choose a favorite", placement = "left"
             )
         ), # end selectInput
-      selectInput("select_gene",
-                  label="RNA-Seq Gene Name (Select 1 or more)",
-                  choices = rownames(pbmc),
-                  multiple = TRUE
-        ) %>% 
-        shinyInput_label_embed(
-          shiny_iconlink() %>%
-            bs_embed_popover(
-              title = "Letter", content = "Choose a favorite", placement = "left"
-            )
-        ),
       selectInput("test_type",
                   label="Select Test Type, default is Wilcox",
                   choices = list('wilcox', 'bimod', 'roc', 't', 'poisson', 'negbinom', 'LR', 'MAST', 'DESeq2'),
@@ -51,7 +42,32 @@ tabPanel("DE Tests",
             bs_embed_popover(
               title = "Letter", content = "Choose a favorite", placement = "left"
             )
-        ),
+        ), # end selectInput
+      numericInput("n_select",
+                  label="Number of Gene Output",
+                  value = 10,
+                  min = 1, 
+                  max = nrow(pbmc)
+      ) %>% 
+        shinyInput_label_embed(
+          shiny_iconlink() %>%
+            bs_embed_popover(
+              title = "Letter", content = "Choose a favorite", placement = "left"
+            )
+        ), # end selectInput
+      
+      selectInput("select_gene",
+                  label="RNA-Seq Gene Name (Select 1 or more) (optional)",
+                  choices = rownames(pbmc),
+                  multiple = TRUE
+      ) %>% 
+        shinyInput_label_embed(
+          shiny_iconlink() %>%
+            bs_embed_popover(
+              title = "Letter", content = "Choose a favorite", placement = "left"
+            )
+        ), # end selectInput
+      downloadButton("downloadData", "Download")
     ),
     # Main Panel
     mainPanel(
